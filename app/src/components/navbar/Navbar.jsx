06c1d2d8c3697c11dcefd3e0867702c21e2fabf3
Juanliendo22 <liendojuan070@gmail.com>
2024-11-25 17:22:18 -0300

    Añadido el login y ajustes al Navbar

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -1,9 +1,40 @@
+import React, { useState } from "react";
 import "./Navbar.css";
+import Button from "../../components/button/Button"; // Importa el componente Button
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
-    return (
-        <h1>Navbar</h1>
-    )
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          {" "}
+          <img src={logo} alt="Logo" />
+        </div>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`navbar-links ${isMenuOpen ? "open" : ""}`}>
+          <li>
+            <a href="#home">Conocenos</a>
+          </li>
+          <li>
+            <a href="#about">Nuestros cursos</a>
+          </li>
+
+          <li>
+            <Button text="Plataforma" /> {/* El botón ahora dirá PLATAFORMA */}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- app/src/pages/index/index.jsx
+++ app/src/pages/index/index.jsx
@@ -1,7 +1,12 @@
-import danceLogo from "../../assets/dancelogo.jpg";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./index.css";
 import Navbar from "../../components/navbar/Navbar";
+import Home from "../../components/Home/Home"; // Importa el nuevo componente Home
+import Login from "../login/login";
+import VideoSection from "../../sections/videoSection/videoSection";
+import danceVideo from "../../assets/dancevideo.mp4";
+import ImgVideoSection from "../../assets/ImgVideoSection.png";
+import "./index.css";
 
 const Index = () => {
   const navigate = useNavigate();
@@ -10,55 +15,25 @@ const Index = () => {
     navigate("/register");
   };
 
-  const navigateToLogin = () => {
-    navigate("/login");
-  };
-
   return (
-    <div>
-    <Navbar></Navbar>
+    <div className="page-container">
+      {/* Contenedor principal */}
+      <Navbar />
       <div className="containerIndex">
-        <a target="_blank">
-          <img src={danceLogo} className="logo" alt="React logo" />
-        </a>
-        <h1>Movimientos del Alma</h1>
-        <p>
-          En nuestra Academia de Danza, creemos que cada paso cuenta, cada
-          movimiento tiene un propósito y que el baile es mucho más que una
-          disciplina: es una forma de vida. Nos dirigimos a todas las personas,
-          sin importar su edad o experiencia, que busquen expresar su
-          creatividad, mejorar su bienestar físico y emocional, o simplemente
-          disfrutar del poder transformador del arte del movimiento. Nuestra
-          misión es ofrecer un espacio donde el arte de la danza se convierta en
-          una experiencia accesible, enriquecedora y transformadora. Aquí, cada
-          estudiante es único, y nos esforzamos por crear un ambiente acogedor y
-          motivador en el que puedan desarrollarse a su propio ritmo, sin
-          presiones, pero con el desafío de superarse constantemente. Contamos
-          con una variedad de estilos de danza, desde el ballet clásico y la
-          danza contemporánea, hasta el hip hop, el jazz y la salsa, para que
-          cada persona pueda encontrar la disciplina que más resuene con su
-          personalidad y objetivos. Nuestros profesores son profesionales
-          apasionados por la enseñanza y el arte de la danza, comprometidos a
-          brindar una formación técnica sólida, pero también a fomentar la
-          creatividad, la confianza y el disfrute del proceso artístico. En la
-          Academia de Danza, también entendemos que la danza es un vehículo para
-          el bienestar integral. No solo es una excelente forma de ejercicio
-          físico, sino que también ayuda a liberar tensiones, mejora la postura,
-          fortalece la coordinación y promueve la conexión con uno mismo y con
-          los demás. Cada clase es una oportunidad para explorar tu propio
-          cuerpo, desatar tu energía y conectar con una comunidad de personas
-          que comparten tu amor por la danza. Te invitamos a unirte a nuestra
-          familia de danza y descubrir lo que significa moverse con libertad,
-          expresar lo que sientes y disfrutar del viaje. Aquí, todos tienen un
-          lugar, porque creemos que la danza es un lenguaje universal que
-          conecta a las personas más allá de las palabras.
-        </p>
-        <div className="card">
-          <button onClick={navigateToLogin}>Iniciar Sesion</button>
-          <button onClick={navigateToRegister}>Registrarse</button>
+        {/* Reemplaza el contenido aquí por el nuevo componente Home */}
+        <Home />
+        <Login />
+
+        {/* Sección del video */}
+        <div className="video-section-container">
+          <VideoSection
+            title="Alma en movimiento"
+            imageSrc={ImgVideoSection}
+            videoSrc={danceVideo}
+          />
         </div>
       </div>
-      </div>
+    </div>
   );
 };
 
