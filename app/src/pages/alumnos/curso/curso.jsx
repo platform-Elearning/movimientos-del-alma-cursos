c0f45807489cb92f0afd274f3328c592715c8549
julimax <julimax951@gmail.com>
2024-11-28 21:26:54 -0300

    alumnos pages

---
+++ app/src/pages/alumnos/curso/curso.jsx
@@ -0,0 +1,9 @@
+import "./curso.css";
+
+const Curso = () => {
+    return (
+        <h1>curso</h1>
+    )
+}
+
+export default Curso;
--- app/src/router/AppRouter.jsx
+++ app/src/router/AppRouter.jsx
@@ -3,6 +3,9 @@ import Index from "../pages/index/index";
 import Login from "../pages/login/login";
 import Register from "../pages/register/register";
 import PageAuxiliar from "../pages/pageAuxiliar/pageAuxiliar";
+import MisCursos from "../pages/alumnos/misCursos/misCursos";
+import Curso from "../pages/alumnos/curso/curso";
+import Clase from "../pages/alumnos/clase/clase";
 
 const AppRouter = () => {
     return (
@@ -11,6 +14,9 @@ const AppRouter = () => {
             <Route path="/login" element={<Login></Login>}></Route>
             <Route path="/register" element={<Register></Register>}></Route>
             <Route path="/pageAuxiliar" element={<PageAuxiliar></PageAuxiliar>}></Route>
+            <Route path="/alumnos/miscursos/:id" element={<MisCursos></MisCursos>}></Route>
+            <Route path="/alumnos/curso/:id" element={<Curso></Curso>}></Route>
+            <Route path="/alumnos/clase/:id" element={<Clase></Clase>}></Route>
         </Routes>
     )
 }
