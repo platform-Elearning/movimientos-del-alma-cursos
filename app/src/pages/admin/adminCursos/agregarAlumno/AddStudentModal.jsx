068bd2bda39b429f07841c1c8cbcbe94fe81674b
julimax <julimax951@gmail.com>
2024-12-15 13:36:30 -0300

    update register student to course

--- app/src/pages/admin/adminCursos/agregarAlumno/AddStudentModal.jsx
+++ app/src/pages/admin/adminCursos/agregarAlumno/AddStudentModal.jsx
@@ -6,22 +6,24 @@ const AddStudentModal = ({ courseId, onClose }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const studentId = e.target[0].value;
-      const enrollmentStatus = "active"; // Puedes ajustar esto según sea necesario
-      const notes = e.target[2].value;
+      const student_dni = e.target[0].value;
+      const notes = e.target[1].value;
+      const modulos_Comprados = e.target[2].value; 
 
       const enrollmentData = {
-        student_id: studentId,
+        dni: Number(student_dni),
         course_id: courseId,
-        enrollment_status: enrollmentStatus,
+        modules_covered: Number(modulos_Comprados),
         notes: notes,
       };
-
+      console.log("enrollmentData", enrollmentData);
       const response = await registerStudentToCourse(enrollmentData);
       alert(`${JSON.stringify(response.message)}`); // Muestra la respuesta en una alerta
       onClose(); // Cerrar el modal después de agregar
     } catch (error) {
       console.error("Error al agregar el alumno al curso:", error);
+      const errorMessage = error.response?.data?.message || error.message || "Error desconocido";
+      alert(`Error al agregar el alumno al curso: ${errorMessage}`); // Muestra el error en una alerta
     }
   };
 
@@ -30,8 +32,8 @@ const AddStudentModal = ({ courseId, onClose }) => {
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <h2>Agregar Alumno al Curso ID: {courseId}</h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="ID del Alumno" required />
-          <input type="text" placeholder="Notas" required value={"active"}/>
+          <input type="text" placeholder="DNI del Alumno" required />
+          <input type="text" placeholder="Notas" required />
           <input type="number" placeholder="modulos comprados" required />
           <button type="submit" className="modal-submit-button">Agregar</button>
           <button type="button" className="modal-close-button" onClick={onClose}>
